39a5920f9571fa2698d735341f77956724697e77
dotkalm <joel@joelholmberg.com>
2019-12-21 19:38:02 -0800

    add some test code

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,36 @@ exports.ping = functions.https.onRequest((request, response) => {
     try{
         console.log(request)
         response.send("ping")
+        }
     } catch(error){
         response.send(error)
     }
+})
+
+exports.addItem = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if(req.method !== 'POST'){
+            return res.status(401).json({
+                message: 'not allowed'
+            })
+        }
+        console.log(req.body)
+        const item = req.body.item
+        database.push({ item });
+        let items = [];
+
+        return database.on('value', (snapshot) => {
+            snapshot.forEach((item) => {
+                items.push({
+                    id: item.key,
+                    items: item.val().item
+                });
+            });
+        res.status(200).json(items)
+        }, (error) => {
+            res.status(error.code).json({
+                message: `Something went wrongo. ${error.message}`
+            })
+        })
+    })
 })
